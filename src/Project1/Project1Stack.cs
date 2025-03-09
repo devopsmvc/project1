820f44b9a0ef1a52702ab5f8cebfa3f80a5d47bc
devopsmvc <devops.mvc@gmail.com>
2025-03-09 13:07:22 +0530

    Initial commit

---
+++ src/Project1/Project1Stack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace Project1
+{
+    public class Project1Stack : Stack
+    {
+        internal Project1Stack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
